221e8f77d227ffd50c71685107828aa38452789d
Satyam <satyam19arya@gmail.com>
2023-06-19 00:03:32 +0530

    changes

--- 20_pointers.cpp
+++ 20_pointers.cpp
@@ -2,22 +2,6 @@
 using namespace std;
 
 int main(){
-    int A[5]={2,4,6,8,10};
-    int *p=A,*q=&A[4];
-    cout<<*p<<endl;
-        
-    p++;
-    cout<<*p<<endl;
-        
-    p--;
-    cout<<*p<<endl;
-    cout<<p<<endl;
-    cout<<p+2<<endl;
-    cout<<*p<<endl;
-    cout<<*(p+2)<<endl;
-    cout<<q-p<<endl;
-    cout<<p-q;
-
     //Pointers basics
     int a = 5;
     int *ptr = &a;
@@ -40,6 +24,38 @@ int main(){
     int *m; //Bad practice
     int *n= 0; //Good practice
     int *v= nullptr; //Good practice
+
+    //Pointers basics (Array)
+
+    int arr[4] = {12, 14, 16, 18};
+    
+    cout<<arr<<endl;  //arr is a pointer to the first location of the array
+    cout<<arr[0]<<endl;
+    cout<<&arr<<endl;
+    cout<<&arr[0]<<endl;
+    cout<<*arr<<endl;
+    cout<<*arr+1<<endl;
+    cout<<*(arr)+1<<endl;
+    cout<<*(arr+1)<<endl;
+    cout<<*(arr+2)<<endl;  // both are same 
+    cout<<arr[2]<<endl;    // both are same (internally converted to *(arr+2))
+    cout<<2[arr]<<endl;    //i[arr] == arr[i]  (internally *(arr+2) == *(2+arr))
+    int i=0;
+    cout<<arr[i]<<endl;
+    cout<<&arr[i]<<endl;
+
+    int *p = arr;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p<<endl;
+    cout<<*(p+2)<<endl;
+    cout<<p[2]<<endl;
+    p=p+2;
+    cout<<p<<endl;
+    // arr= arr+2;       //compilation error because arr is pointing to memory location of the first element of the array (base address) & you can't change the base address but we can use pointer arithmetic to access other elements of the array
+    // cout<<arr<<endl;
+    
+    //dereference means uss address par jo value padi hai wo bata dho
     
     return 0;     
-}  
+}
