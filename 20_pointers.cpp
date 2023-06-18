ddc2adad55f579176d8c0af8c2e00dc10cf667b9
Satyam <satyam19arya@gmail.com>
2023-06-18 20:14:10 +0530

    changes

--- 20_pointers.cpp
+++ 20_pointers.cpp
@@ -17,7 +17,29 @@ int main(){
     cout<<*(p+2)<<endl;
     cout<<q-p<<endl;
     cout<<p-q;
+
+    //Pointers basics
+    int a = 5;
+    int *ptr = &a;
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+    cout<<&ptr<<endl;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(ptr)<<endl;
+    
+    a=a+1;
+    cout<<a<<endl;
+    *ptr=*ptr+1;
+    cout<<*ptr<<endl;
+    cout<<a<<endl;
+    ptr=ptr+2;
+    cout<<ptr<<endl;
+    
+    int *m; //Bad practice
+    int *n= 0; //Good practice
+    int *v= nullptr; //Good practice
     
     return 0;     
-}
-    
+}  
